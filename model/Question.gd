9f197fe3190195ff2dbd86af0d48cb92b7c1eeee
Francesca Nueva <fnueva@uw.edu>
2026-05-03 00:41:03 -0700

    Built script for Question.gd so it handles the 3 types of questions.

--- model/Question.gd
+++ model/Question.gd
@@ -1,11 +1,15 @@
 extends Node
 
+# Model: Handles the data and logic for the 3 types of quetions
+# Question Types: Mulitple Choice, true/false, and short answer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+enum QuestionType {MULTIPLE_CHOICE, TRUE_FALSE, SHORT_ANSWER }
 
+var current_type = QuestionType.MULTIPLE_CHOICE
+var question_text: String = ""
+var correct_answer: String = "" # This would store the correct answer
+var options: Array = [] #Use this for multiple choice selections
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# Loads the data 
+
+# Checks player's input
