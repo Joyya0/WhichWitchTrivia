6a46a4964254cf70a1d35aa41be502641a53c485
Anna O <ajoy0112@outlook.com>
2026-06-01 10:54:02 -0700

    fixed duplicate questions

--- view/QuestionManager.gd
+++ view/QuestionManager.gd
@@ -7,6 +7,11 @@ var question_tables = [
 	"true_false",
 	"short_answer"
 ]
+var used_questions = {
+	"multiple_choice": [],
+	"true_false": [],
+	"short_answer": []
+}
 
 func _ready():
 
@@ -20,39 +25,86 @@ func _ready():
 
 	print("QuestionManager loaded")
 
-
 func get_random_question():
 
-	# Pick random table
 	var table = question_tables.pick_random()
 
-	print("Selected table: ", table)
+	return get_question_from_table(table)
 
-	# SQL query
-	var query = "SELECT * FROM %s ORDER BY RANDOM() LIMIT 1" % table
 
-	print("Running query: ", query)
+func get_random_question_from_other_table():
+
+	var available_tables = []
+
+	for table in question_tables:
+
+		var query = ""
+
+		if used_questions[table].is_empty():
+
+			query = "SELECT COUNT(*) as count FROM %s" % table
+
+		else:
+
+			var ids = []
+
+			for id in used_questions[table]:
+				ids.append(str(id))
+
+			query = "SELECT COUNT(*) as count FROM %s WHERE id NOT IN (%s)" % [
+				table,
+				",".join(ids)
+			]
+
+		db.query(query)
+
+		if db.query_result[0]["count"] > 0:
+			available_tables.append(table)
+
+	if available_tables.is_empty():
+
+		print("All questions have been used.")
+		return null
+
+	var new_table = available_tables.pick_random()
+
+	return get_question_from_table(new_table)
+
+func get_question_from_table(table):
+
+	var query = ""
+
+	if used_questions[table].is_empty():
+
+		query = "SELECT * FROM %s ORDER BY RANDOM() LIMIT 1" % table
+
+	else:
+
+		var ids = []
+
+		for id in used_questions[table]:
+			ids.append(str(id))
+
+		query = "SELECT * FROM %s WHERE id NOT IN (%s) ORDER BY RANDOM() LIMIT 1" % [
+			table,
+			",".join(ids)
+		]
 
 	db.query(query)
 
-	if db.query_result.size() == 0:
+	if db.query_result.is_empty():
 		return null
 
-	# Get first row
 	var question = db.query_result[0]
 
-	# Add type info
-	match table:
+	used_questions[table].append(question["id"])
 
+	match table:
 		"multiple_choice":
 			question["type"] = "mc"
-
 		"true_false":
 			question["type"] = "tf"
-
 		"short_answer":
 			question["type"] = "sa"
-
-	print("Loaded question: ", question)
 
 	return question
